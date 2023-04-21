22a8443a8e5338c065c9eca18a8cb4b4a9e01f38
Keaton <keatonmaruyali@gmail.com>
2023-04-21 11:16:58 -0400

    Improve functionality of Periodic Table

--- src/components/PeriodicTableGame/PeriodicTableGame.jsx
+++ src/components/PeriodicTableGame/PeriodicTableGame.jsx
@@ -1,48 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import './periodicTableGame.css';
-import { NavLink } from "react-router-dom";
 
 import { elements } from "../../data/_data";
 
 
 const generateRandomNumbers = (times) => {
-    const randoms = []
+  const randoms = []
 
-    for (let i = 0; i < times; i++) {
-        randoms.push(Math.floor(Math.random() * (118 - 0) + 0))
-    }
-
-    return randoms
+  for (let i = 0; i < times; i++) {
+    randoms.push(Math.floor(Math.random() * (117 - 0) + 0))
+  }
+  return randoms
 };
 
 const shuffle = () => {
-    let array = [0, 1, 2, 3]
-    let currentIndex = array.length,  randomIndex;
-  
-    while (currentIndex != 0) {
-  
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array[1];
+  let array = [0, 1, 2, 3]
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+    array[randomIndex], array[currentIndex]];
+  } 
+  return array[1];
 };
 
 const PeriodicTableGame = () => {
   const [answer, setAnswer] = useState('');
   const [mcQ, setMcQ] = useState({});
   const [showStart, setShowStart] = useState(true);
-  const [showPopUp, setShowPopUp] = useState(false);
-  const [correct, setCorrect] = useState(false);
+  const [correct, setCorrect] = useState(true);
+  const [score, setScore] = useState(0);
 
   const generateQuestions = () => {
     setShowStart(false);
-    setShowPopUp(false);
-    setCorrect(false);
+    setCorrect(true);
     setAnswer('');
+
     const randNum = generateRandomNumbers(4);
     const ranIndex = shuffle();
 
@@ -51,24 +47,24 @@ const PeriodicTableGame = () => {
 
     for (const [index, value] of randNum.entries()) {
       if (index == ranIndex) {
-          answerOptions.push({
-              'answerText': elements[value].name,
-              'isCorrect': true,
-          })
-          questionDetails = elements[value].symbol;
+        answerOptions.push({
+          'answerText': elements[value].name,
+          'isCorrect': true,
+        })
+        questionDetails = elements[value].symbol;
       } else {
-          answerOptions.push({
-              'answerText': elements[value].name,
-              'isCorrect': false,
-          })
+        answerOptions.push({
+          'answerText': elements[value].name,
+          'isCorrect': false,
+        })
       }
     };
   
     var questions = [
       {
-          'question': 'What is the name of the compound with this symbol?',
-          'questionDetails': questionDetails,
-          'answerOptions': answerOptions,
+        'question': 'What is the name of the compound with this symbol?',
+        'questionDetails': questionDetails,
+        'answerOptions': answerOptions,
       }
     ];
     setMcQ(questions);
@@ -78,61 +74,56 @@ const PeriodicTableGame = () => {
     var inputs = document.getElementsByTagName('input');
 
     for(var i = 0; i < inputs.length; i++) {
-        inputs[i].checked=false
-        }
+      inputs[i].checked=false
+    }
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log('Checking answers... ', mcQ[0].answerOptions[answer].isCorrect);
     if (mcQ[0].answerOptions[answer].isCorrect) {
-        console.log('Right Answer');
-        setCorrect(true);
-        generateQuestions();
+      setScore(score+1);
+      generateQuestions();
     } else {
-        console.log('Wrong answer!');
-        setShowPopUp(true);
+      setCorrect(false);
     }
   };
 
   useEffect(() => {
-    console.log('New MCQ: ', mcQ);
     resetInput();
   }, [mcQ]);
 
   return (
     <div>
-        <h2>Game</h2>
-        {
-            showStart && <button className='start-button' onClick={generateQuestions}>Start</button>
-        }
-        {
-            Object.keys(mcQ).length !== 0 && (
-            <div className='periodic_table-game'>
-                <p>
-                    {mcQ[0].question} - {mcQ[0].questionDetails}
-                </p>
-                <form name='answerOptions'className="answer_div" onSubmit={handleFormSubmit}>
-                    {mcQ[0].answerOptions.map((answerOption, index) => (
-                        <div>
-                            <input
-                                id={index}
-                                type="radio"
-                                checked={index === answer}
-                                onChange={() => setAnswer(index)}
-                                value={answerOption.answerText}
-                                name="answerOption"
-                            />
-                            {answerOption.answerText}
-                        </div>
-                    ))}
-                    <input disabled={answer===''} className='game-button' type="submit" value="Submit" />
-                    <div className='game_message_content'>
-                        {!correct && <p>Not quite. Please try again!</p>}
-                    </div>
-                </form>
-            </div>)
-        }
+      {
+        showStart && <button className='start-button' onClick={generateQuestions}>Start</button>
+      }
+      {
+        Object.keys(mcQ).length !== 0 && (
+        <div className='periodic_table-game'>
+        <h3>Score: {score}</h3>
+        <p>{mcQ[0].question} - {mcQ[0].questionDetails}</p>
+            <form name='answerOptions'className="answer_div" onSubmit={handleFormSubmit}>
+            {mcQ[0].answerOptions.map((answerOption, index) => (
+                <div>
+                <input
+                    id={index}
+                    type="radio"
+                    checked={index === answer}
+                    onChange={() => setAnswer(index)}
+                    value={answerOption.answerText}
+                    name="answerOption"
+                />
+                {answerOption.answerText}
+                </div>
+            ))}
+            <input disabled={answer===''} className='game-button' type="submit" value="Submit" />
+            <div className='game_message_content'>
+                {!correct && <p>Not quite. Please try again!</p>}
+            </div>
+          </form>
+        </div>
+        )
+      }
     </div>
   );
 }
--- src/pages/PeriodicTableOfElements/PeriodicTableOfElements.jsx
+++ src/pages/PeriodicTableOfElements/PeriodicTableOfElements.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PeriodicTable from '../../components/PeriodicTable/PeriodicTable';
 import PeriodicTableGame from '../../components/PeriodicTableGame/PeriodicTableGame';
-import { NavLink } from 'react-router-dom';
 import './periodicTableOfElements.css';
 
 
 const PeriodicTableOfElements = () => {
-  const [displayTable, setDisplayTable] = useState(false);
+  const [displayTable, setDisplayTable] = useState(true);
   const [displayGame, setDisplayGame] = useState(false);
 
   const resetDisplay = () => {
@@ -31,15 +30,13 @@ const PeriodicTableOfElements = () => {
   };
 
   return (
-    <div>
+    <div className='periodic_table-page-container'>
         <h2>Periodic Table of Elements</h2>
-        <div className='periodic_table-navbar-container'>
-            <nav className='periodic_table-nav'>
-              <div id="periodic-table" onClick={handleOnClick}>Periodic Table</div>
-              <div id="game" onClick={handleOnClick}>Game</div>
-            </nav>
-        </div>
-        <div>  
+        <nav className='periodic_table-nav'>
+          <div id="periodic-table" onClick={handleOnClick}>Periodic Table</div>
+          <div id="game" onClick={handleOnClick}>Game</div>
+        </nav>
+        <div className='periodic_table-content'>  
             {displayTable && (<PeriodicTable />)}
             {displayGame && (<PeriodicTableGame />)}
         </div>
